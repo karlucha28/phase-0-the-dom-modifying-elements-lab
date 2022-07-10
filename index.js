e61334be48da5c496ec4c89306a8ba5a41d5b37f
Karl Savinskiy <karlsavinskiy@gmail.com>
2022-07-09 17:54:42 -0700

    Finished

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+document.body.innerHTML = "<h1 id='victory'>YOUR-NAME is the champion</h1>";
+"<h1 id='victory'>YOUR-NAME is the champion</h1>"
+"<h1 id='victory'>YOUR-NAME is the champion</h1>";
+const newHeader = document.getElementById("victory");
